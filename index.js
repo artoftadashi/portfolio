1a4ca49532470c03259a8c75cdd79e824d51019c
artoftadashi <samuelolanrewaju05@gmail.com>
2024-03-04 16:42:19 +0000

    added the experience section

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ const spotlightEl = document.querySelector("#spotlight");
 function handleMouseMove(event) {
     const { clientX, clientY } = event;
     
-    spotlightEl.style.background = `radial-gradient(circle at ${clientX}px ${clientY}px, #00000000 10px, #011411 350px)`;
+    spotlightEl.style.background = `radial-gradient(circle at ${clientX}px ${clientY}px, ##c587ff 10px, ##e3c6ff 350px)`;
 }
 
 document.addEventListener("mousemove", handleMouseMove)
